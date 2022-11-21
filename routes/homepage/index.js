abeca6a0e984c460d87f5818b0c61f581916b43b
Ashwani Kumar <ashwanikr160@gmail.com>
2022-11-22 03:29:30 +0530

    added homepage

--- routes/homepage/index.js
+++ routes/homepage/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile('../../view');
+  console.log(req.cookies);
+  res.sendFile(path.join(__dirname, '../../view/index.html'));
 });
 
 module.exports = router;
